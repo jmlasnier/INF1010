ae7f0ef42c30734a4b6e5743bf416f0b0942cab5
Jean-Michel Lasnier <j-m.lasnier@hotmail.com>
2018-02-08 22:21:37 -0500

    update TP2 2018-02-08-12h30/

---
+++ INF1010_TP/TP2/TP2_1905682_1582075 (2018-02-08-12h30)/TP2/Panier.cpp
@@ -0,0 +1,75 @@
+/********************************************
+* Titre: Travail pratique #2 - Panier.cpp
+* Date: 25 janvier 2018
+* Auteur: Mohammed Esseddik BENYAHIA & Timothée CHAUVIN
+*******************************************/
+
+#include "Panier.h"
+
+Panier::Panier() :	totalAPayer_{ 0 }
+{
+}
+
+Panier::~Panier()
+{
+}
+
+// methodes d'accès
+vector <Produit *>  Panier::obtenirContenuPanier()const
+{
+	return contenuPanier_;
+}
+
+int Panier::obtenirNombreContenu() const
+{
+	return contenuPanier_.size;
+}
+
+
+double Panier::obtenirTotalApayer() const
+{
+	return totalAPayer_;
+}
+
+void Panier::modifierTotalAPayer(double totalAPayer)
+{
+	totalAPayer_ = totalAPayer;
+}
+
+// méthodes de modification
+
+// autres méthodes
+void Panier::ajouter(Produit* prod)		//Implementation modif pour vector
+{
+	contenuPanier_.push_back(prod);
+	totalAPayer_ += prod->obtenirPrix();
+}
+
+void Panier::livrer()
+{
+	contenuPanier_.clear();
+	totalAPayer_ = 0;
+}
+
+Produit * Panier::trouverProduitPlusCher()
+{
+	Produit* prod = nullptr;
+	int nbProd = obtenirNombreContenu()¸;
+
+	if (nbProd != 0)
+	{
+		for (int i = 0; i < nbProd; i++)
+		{
+			if (*contenuPanier_[i] > *prod)
+				prod = contenuPanier_[i];
+		}
+	}
+	return prod;
+}
+	// TODO: Implementez la methode
+	//Surcharge de l'opérateur <<
+	
+ostream& operator<<(ostream& os, Panier& panier)
+{
+	return os << panier.obtenirContenuPanier << "/" << panier.obtenirTotalApayer << endl;
+}
---
+++ INF1010_TP/TP2/TP2_1905682_1582075 (2018-02-08-12h30)/TP2/Rayon.cpp
@@ -0,0 +1,70 @@
+/********************************************
+* Titre: Travail pratique #2 - Rayon.cpp
+* Date: 25 janvier 2018
+* Auteur: Timoth�e CHAUVIN
+*******************************************/
+
+#include "Rayon.h"
+
+Rayon::Rayon(const string& cat) :
+	categorie_{ cat }
+{
+}
+
+Rayon::~Rayon()
+{
+}
+
+// Methodes d'acces
+string Rayon::obtenirCategorie() const
+{
+	return categorie_;
+}
+
+vector <Produit *> Rayon::obtenirTousProduits() const
+{
+	return tousProduits_;
+}
+
+int Rayon::obtenirCapaciteProduits() const
+{
+	return tousProduits_.capacity();
+}
+
+int Rayon::obtenirNombreProduits() const
+{
+	return tousProduits_.size();
+}
+
+// Methodes de modification
+void Rayon::modifierCategorie(const string& cat)
+{
+	categorie_ = cat;
+}
+
+//ajouterProduit avec la surcharge d'operateur +=
+ Rayon& Rayon::operator+=(Produit const& produit)
+{
+	obtenirTousProduits().push_back(produit);
+	return *this;
+}
+
+
+//Methode de modification compterDoublons
+int Rayon::compterDoublons(const Produit& produit)
+{
+	int doublons;
+	for (int i = 0; i < obtenirNombreProduits(); i++)
+	{
+		if (*tousProduits_[i] == produit)
+			doublons++;
+	}
+	return doublons;
+}
+
+
+//Surcharge de l'operateur <<
+ostream& operator<<(ostream& os, Rayon& rayon)
+{
+	return os << rayon.obtenirCategorie << "/" << rayon.obtenirTousProduits <<  endl;
+}
